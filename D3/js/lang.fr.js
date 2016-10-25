eead9cda70f63052013fbd88eeaa45b8334ef051
Paul Ronga <paul.ronga@laposte.net>
2016-10-25 17:06:37 +0200

    Implement age spans in tooltip

--- D3/js/lang.fr.js
+++ D3/js/lang.fr.js
@@ -7,4 +7,10 @@ lang = {
     in : "à",
     notAvailable : "Pas de données pour ",
     newTown : "Peut-être est-ce une nouvelle commune",
+    from: "de",
+    to: "à",
+    years_old: "ans",
+    center: [6.6, 46.6],
+    /* template literals: fail */
+    /* from_to : `de ${fromAge} à ${toAge}` */
 };
